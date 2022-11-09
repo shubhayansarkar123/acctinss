fe1261690646db3efeb9154d8a25875b3dc9a18c
shubhayan <sarkarsar123@gmail.com>
2022-11-09 17:10:58 +0530

    final2

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,7 +3,7 @@ import os
 
 import sys
 
-sys.path.append(r'C:\Users\ACER\Downloads\Day04-Docker-20221009T052040Z-001\Day04-Docker\Hello World\Hello World')
+sys.path.append(r'./Hello World')
 from app import home
 def test_index():
     assert home()=='Hello World'
